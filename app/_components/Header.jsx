1f58a25e91875942481e81c3fa167eab0fdb4465
rohit27-2 <gaddamrohitgoud@gmail.com>
2024-07-25 17:39:17 +0530

    Updated Logo

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
                 <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
                     <div className="flex h-16 items-center justify-between">
                         <div className="flex-1 md:flex md:items-center md:gap-12">
-                            <a className="block text-teal-600" href="#">
+                            <a className="block text-blue-600" href="#">
                                 <span className="sr-only">Home</span>
                                 <svg className="h-8" viewBox="0 0 28 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path
