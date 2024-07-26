8bcf919619b43583cc0b19b6bb4cbe98c622ac69
rohit27-2 <gaddamrohitgoud@gmail.com>
2024-07-27 00:06:18 +0530

    Added master plan

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -42,6 +42,59 @@ function ProjectPage() {
                         </div>
                     </dl>
                 </div>
+                <section>
+  <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
+    <div className="max-w-3xl">
+      <h2 className="text-4xl font-bold sm:text-5xl">
+        <span className='text-blue-700'>Master </span>
+         Plan
+      </h2>
+    </div>
+
+    <div className="mt-8 grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+      <div className="relative h-64 overflow-hidden sm:h-80 lg:h-full">
+        <img
+          alt=""
+          src="masterplan.jpeg"
+          className="absolute inset-0 h-full w-full object-cover"
+        />
+      </div>
+
+      <div className="lg:py-16">
+        <div className="space-y-4 text-gray-600">
+          <p>
+            Indoor
+          </p>
+          
+          <ol >
+            <li>Business Centre and Co-Working Space</li>
+            <li>Swimming Pool with Pool Deck</li>
+            <li>Air-conditioned Gymnasium</li>
+            <li>Yoga & Meditation Room</li>
+            <li>Music/Dance/Aerobics Room</li>
+            <li>Crèche & Toddlers Play Area</li>
+            <li>Community Hall</li>
+            <li>Guest Room for Common Use</li>
+            <li>Society Administration Office</li>
+            <li>Common Toilet Block</li>
+            <li>Maid’s Area</li>
+            Paddle Pool
+Steam and Sauna
+Spa
+Salon
+Mini-theatre
+Ladies K.T. Room
+Indoor Games Room
+Kitchen
+Cafeteria
+Grocery Store
+Driver’s Area
+          </ol>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 
 
 
